b2dabfee74c6605ab63c220eb8b03c9728c9a3d3
Ivy Xue <i.xue@sap.com>
2019-08-24 17:44:07 +0800

    add lombok to use slf4j log

--- zheng-commons/src/main/java/com/ivy/zheng/commons/base/BaseServiceImpl.java
+++ zheng-commons/src/main/java/com/ivy/zheng/commons/base/BaseServiceImpl.java
@@ -1,85 +1,74 @@
 package com.ivy.zheng.commons.base;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.lang.reflect.Method;
 import java.util.List;
 
+@Slf4j
 public class BaseServiceImpl<Mapper, Record, Example> implements BaseService<Record, Example> {
 
     private Mapper mapper;
 
-    @Override
     public long countByExample(Example example) {
         try {
             Method countByExample = mapper.getClass().getDeclaredMethod("countByExample", example.getClass());
             Object result = countByExample.invoke(mapper, example);
             return Long.valueOf(String.valueOf(result));
         } catch (Exception ex) {
-
+            log.error("countByExample exception", ex);
         }
         return 0;
     }
 
-    @Override
     public int deleteByExample(Example example) {
         return 0;
     }
 
-    @Override
     public int deleteByPrimaryKey(Integer logId) {
         return 0;
     }
 
-    @Override
     public int insert(Record record) {
         return 0;
     }
 
-    @Override
     public int insertSelective(Record record) {
         return 0;
     }
 
-    @Override
     public List<Record> selectByExampleWithBLOBs(Example example) {
         return null;
     }
 
-    @Override
     public List<Record> selectByExample(Example example) {
         return null;
     }
 
-    @Override
     public Record selectByPrimaryKey(Integer logId) {
         return null;
     }
 
-    @Override
     public int updateByExampleSelective(Record record, Example example) {
         return 0;
     }
 
-    @Override
     public int updateByExampleWithBLOBs(Record record, Example example) {
         return 0;
     }
 
-    @Override
     public int updateByExample(Record record, Example example) {
         return 0;
     }
 
-    @Override
     public int updateByPrimaryKeySelective(Record record) {
         return 0;
     }
 
-    @Override
     public int updateByPrimaryKeyWithBLOBs(Record record) {
         return 0;
     }
 
-    @Override
     public int updateByPrimaryKey(Record record) {
         return 0;
     }
